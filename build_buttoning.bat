23cc437d7e18865d30982cc24f74307334f5bb3d
Haves Irfan <20160532+ha-ves@users.noreply.github.com>
2025-11-20 00:28:00 +0900

    move library dirs to env var

--- build_buttoning.bat
+++ build_buttoning.bat
@@ -1,6 +1,5 @@
 colcon --log-level debug build --merge-install --symlink-install ^
-  --base-paths src --packages-up-to buttoning_system ^
-  --cmake-args -DBUILD_TESTING=OFF -DBUILD_TESTS=OFF -DENABLE_TESTS=OFF -DWITH_TESTS=OFF ^
-  -Drealsense2_DIR="C:\Users\admin\source\repos\librealsense\install_l515\lib\cmake\realsense2" ^
-  -DBoost_DIR="C:\local\boost_1_74_0\lib64-msvc-14.1\cmake\Boost-1.74.0" ^
-  -DCMAKE_CONFIGURATION_TYPES=RelWithDebInfo -DCMAKE_BUILD_TYPE=RelWithDebInfo -DPython3_FIND_VIRTUALENV=ONLY
+  --base-paths src --packages-up-to buttoning_system --cmake-args %ENV_CMAKE_ARGS% ^
+  -DBUILD_TESTING=OFF -DBUILD_TESTS=OFF -DENABLE_TESTS=OFF -DWITH_TESTS=OFF ^
+  -DCMAKE_CONFIGURATION_TYPES=RelWithDebInfo -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+  -DPython3_FIND_VIRTUALENV=ONLY
